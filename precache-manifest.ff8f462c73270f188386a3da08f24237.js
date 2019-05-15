edc8ab88c08ae6ce285b80f53a3496912f21afe4
Jonghoon <huni452@naver.com>
2019-05-15 23:40:43 +0900

    Updates

---
+++ precache-manifest.ff8f462c73270f188386a3da08f24237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e64418d6caa6a1f99c1b",
+    "url": "/react-messenger-client/static/js/runtime~main.fa88cfe1.js"
+  },
+  {
+    "revision": "59aa3f41daab803df8e4",
+    "url": "/react-messenger-client/static/js/main.2de245b7.chunk.js"
+  },
+  {
+    "revision": "de62a3d50b94b8e2b589",
+    "url": "/react-messenger-client/static/js/2.5f1906fd.chunk.js"
+  },
+  {
+    "revision": "59aa3f41daab803df8e4",
+    "url": "/react-messenger-client/static/css/main.3f55c204.chunk.css"
+  },
+  {
+    "revision": "6ecdc32a276d97ad904711069c5973e2",
+    "url": "/react-messenger-client/index.html"
+  }
+];
